b5d1a691efc5f9ff81a7d0dbf55a4b6acba735cd
Sarah Dufays <pl247711@ahschool.com>
2021-02-01 00:34:37 +0000

    Update sidebar.js

--- CodeArtCompetition2021/dist/components/sidebar.js
+++ CodeArtCompetition2021/dist/components/sidebar.js
@@ -1,12 +1,3 @@
-/*!
- * # Semantic UI - Sidebar
- * http://github.com/semantic-org/semantic-ui/
- *
- *
- * Released under the MIT license
- * http://opensource.org/licenses/MIT
- *
- */
 
 ;(function ($, window, document, undefined) {
 
